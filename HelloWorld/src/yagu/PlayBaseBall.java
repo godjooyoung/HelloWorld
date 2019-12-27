1140fc7f8cce80a03050385a708a9dd10a762126
JOO <JOO@DESKTOP-R2ICKPJ>
2019-12-27 23:46:13 +0900

    숫자 중복없이 나오게 유효성 검사 어찌할지 생각해보기  일에서 구까지만 입력 가능하도록 수정해보기

--- HelloWorld/src/yagu/PlayBaseBall.java
+++ HelloWorld/src/yagu/PlayBaseBall.java
@@ -3,50 +3,54 @@ package yagu;
 import java.util.Scanner;
 
 public class PlayBaseBall {
-public static void main(String[] args) {
-	boolean run = true;
-	int[] users = new int[3];
-	int[] secret =  new int[3];
-	
-	Scanner scanner = new Scanner(System.in);
-	
-	System.out.println("야구게임 시작");
-	for (int i = 0 ; i<3; i++) {
-		secret[i] = (int)(Math.random()*9)+1; 
-	}
-	for (int i = 0 ; i<3; i++) {
-	System.out.println(secret[i]);
-	}
-	
-	while(run) {
-	
-		System.out.println("1~9사이의 숫자를 입력해 주세요 : ");
-		for(int i = 0; i<3; i++) {
-			users [i] =scanner.nextInt();
-			scanner.nextLine();
+	public static void main(String[] args) {
+		boolean run = true;
+		int[] users = new int[3];
+		int[] secret = new int[3];
+		int strike = 0;
+		int ball = 0;
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("야구게임이 시작되었습니다.");
+		for (int i = 0; i < 3; i++) {
+			secret[i] = (int) (Math.random() * 9) + 1;
+		}
+
+		for (int i = 0; i < 3; i++) {
+			System.out.print(secret[i] + ".");
+		}
+
+		System.out.println(" ");
+
+		while (run) {
+			for (int i = 0; i < 3; i++) {
+				System.out.println("1 ~ 9 사이의 숫자를 입력해 주세요 : ");
+				users[i] = scanner.nextInt();
+				scanner.nextLine();
+			}
+			System.out.println("당신이 입력한 숫자는 : " + users[0] + ", " + users[1] + ", " + users[2] + " 입니다.");
+
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j++) {
+					if (i == j && users[i] == secret[j]) {
+						strike++;
+					} else if (i != j && users[i] == secret[j]) {
+						ball++;
+					}
+					
+					if (strike == 3) {
+						System.out.println("축하합니다. 정답입니다.");
+						run = (false);
+					}
+				}
+			}
+			System.out.println(strike + " strike!");
+			System.out.println(ball + " ball!");
+			strike = 0;
+			ball =0;
+			
+			
 		}
 		
-		System.out.println("당신이 입력한 숫자는 : " + users[0] + ", " + users[1] + ", " + users[2]);
-						
-		for (int i=0; i<3; i++) {
-			int strike = 0;
-			int ball = 0;
-			int ct=0;
-		if (users[i] == secret[i]) {
-		
-			strike++;
-			System.out.println(strike + "스트라이크 !");
-		}else if(users[0] == secret[i]) {
-			ball++;
-			System.out.println(ball + "볼!");
-		}else if(users[1] == secret[i]) {
-			ball++;
-			System.out.println(ball + "볼!");
-		}else if(users[2] == secret[i]) {
-			ball++;
-			System.out.println(ball + "볼!");
-		}
-}
-}
-}
+	}
 }
