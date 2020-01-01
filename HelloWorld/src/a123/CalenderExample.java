c68a660ab5b7f9654200540a45d6e50d79958f14
JOO <JOO@DESKTOP-R2ICKPJ>
2020-01-02 02:44:35 +0900

    jj

--- HelloWorld/src/a123/CalenderExample.java
+++ HelloWorld/src/a123/CalenderExample.java
@@ -14,18 +14,42 @@ public class CalenderExample {
 
 		cal.set(2019, 0, 1);
 
-		createCal(2019, 5);
+		createCal(2020, 1);
 	}
+
 //2020년 2월 달력 구하기
 	public static void createCal(int year, int month) {
 		Calendar cal = Calendar.getInstance();
 		System.out.println(" ‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡ 달  력  ‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡ ");
 		String week[] = { "| 일  |", "| 월  |", "| 화  |", "| 수  |", "| 목  |", "| 금  |", "| 토  |" };
-		int years = cal.get(year);
-		int months =cal.get(month);
-		int days []=new int []; 
+		year = 2020;
+		month = 1;
+		int dateSet = 1;
+		cal.set(year, month, dateSet);
+		System.out.println("설정한 마지막 일 :" + cal.getActualMaximum(Calendar.DAY_OF_MONTH));
+		System.out.println("설정한 오늘의 요일 :" + cal.get(Calendar.DAY_OF_WEEK));
+
+		int calbody[][] = new int[7][5];
+		int day = 2;
+		for (int i = 0; i <= cal.getActualMaximum(Calendar.DAY_OF_MONTH); i++) {
+			for (int x = 1; x < 6; x++) {
+				for (int y = 0; y < 4; y++)
+					calbody[x][y] = day;
+					day= day+1;
+			}
+		}
+
 		for (int i = 0; i < week.length; i++) {
 			System.out.print(" " + week[i]);
 		}
+		System.out.println();
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 4; j++) {
+				System.out.print(calbody[i][j] + " *");
+				
+			}
+			System.out.println();
+		}
+
 	}
 }
